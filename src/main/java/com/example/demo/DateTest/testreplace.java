2fe32b1a1832e5ae64ddf2b5cdeae85090712236
learnxin <591367010@qq.com>
2019-05-16 10:48:04 +0800

    默认的

---
+++ src/main/java/com/example/demo/DateTest/testreplace.java
@@ -0,0 +1,11 @@
+package com.example.demo.DateTest;
+
+public class testreplace {
+
+    public static void main(String[] args) {
+        String str="ssseggwegggjw";
+        String s = str.replaceAll("s", "a").replaceAll("gg", "GG");
+        System.out.println(s);
+    }
+
+}
